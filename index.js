46023a48d462fe7e750adbfef75c5b4ccdb47302
faraz ismail <admin@samwells-MacBook-Air.local>
2017-11-09 11:31:24 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*!
+ * express
+ * Copyright(c) 2009-2013 TJ Holowaychuk
+ * Copyright(c) 2013 Roman Shtylman
+ * Copyright(c) 2014-2015 Douglas Christopher Wilson
+ * MIT Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/express');
+var express = require('express')
+  var app = express()
+    app.get('/', function (req, res) {
+    res.send('Hello World!')
+  })
+
+    app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  })
